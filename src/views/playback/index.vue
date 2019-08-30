d59caa99920c11ba1b3b98f72c5598cbd0976a6d
hemeng <993031732@qq.com>
2019-08-30 15:02:15 +0800

    ss

--- src/views/playback/index.vue
+++ src/views/playback/index.vue
@@ -21,38 +21,44 @@
       </v-playbar>
 
       <div class="grid-control">
-        <v-gridbar @selectGridEvent="handleSelectGrid"></v-gridbar>  
+        <v-gridbar @selectGridEvent="handleSelectGrid"></v-gridbar>        
       </div>
     </div>
-     
+    <div class="playback-right">
+      <button @click="dialogVisible=true">弹出框</button>
+      <el-dialog title="表格示例" :visible.sync="dialogVisible">        
+      </el-dialog>
+    </div>
   </div>
 </template>
 
 <script>
   import vPlaybar from '@/components/video/playbar'
-  import vGridbar from '@/components/video/gridbar';
+  import vGridbar from '@/components/video/gridbar'
   import vPlayer from '@/components/video/player'
   import vPlayergrid from '@/components/video/playergrid2x2'
-  
+    
   export default {
     name: '',
     components: {
       vPlaybar,
       vGridbar,
       vPlayer,
-      vPlayergrid      
+      vPlayergrid
     },
     data() {
       return {
+        dialogVisible: false,
+
         gridNum: '1x1',
         aspectRatio: '7:3',
         playState: 'play',
-        videoUrl: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
+        videoUrl: 'http://192.168.1.76:8089/video/linda.mp4',
 
         videoList: [{
           guid: '92D0722D-D4C3-4FCD-8FDF-029342FE4E05',
           caption: 'linda',
-          videoUrl: 'http://192.168.1.76:8090/video/linda.mp4',
+          videoUrl: 'http://192.168.1.76:8089/video/linda.mp4',
         },{
           guid: '65D4C246-DB25-49E3-94CA-5FB6FABAA0FF',
           caption: 'big_buck_bunny',
@@ -60,12 +66,20 @@
         },{
           guid: '12092F87-9B93-43C1-B597-1A1BCE076E12',
           caption: 'toy',
-          videoUrl: 'http://192.168.1.76:8090/video/toy.mp4',
+          videoUrl: 'http://192.168.1.76:8089/video/toy.mp4',
         },{
           guid: '4366EC3D-B7A6-4F42-BF64-82CDF547498B',
           caption: '复联4',
-          videoUrl: 'http://192.168.1.76:8090/video/fl4.mp4',
-        }]
+          videoUrl: 'http://192.168.1.76:8089/video/fl4.mp4',
+        }],
+
+        columnList: [
+          {title: '机构编号', key: 'number', width: 85, textAlign: 'left'},
+          {title: '机构名称',key: 'name',width: 292,textAlign: 'left',textLine: 2,selectText: true},
+          {title: '类型',key: 'type',width: 180,textAlign: 'center'},
+          {title: '状态',key: 'brand',width: 82,textAlign: 'center'}
+        ],
+        tableData: []
       }
     },    
     created: function() {
@@ -88,8 +102,7 @@
   .playback-container{
     width: 100%;    
     display: flex;
-    flex-wrap: wrap;
-    flex-direction: column;
+    flex-wrap: wrap;    
     justify-content: space-between;    
   }
   .playback-content{
